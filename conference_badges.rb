e1631708a0814f3fef34cf070b3d546afb03ed2b
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-19 00:19:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,10 +21,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  assign_rooms(attendees).each do |room|
-    puts room
-  end
   batch_badge_creator(attendees) do |name|
     puts name
+  end
+  assign_rooms(attendees).each do |room|
+    puts room
   end
 end
