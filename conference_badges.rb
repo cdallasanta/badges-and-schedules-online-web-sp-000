7662b81a0efd8f2620c8acdbfb2e38f62681904b
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-19 00:11:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,12 @@ def batch_badge_creator(attendees)
   attendees.each do |name|
 
 
-def assign_rooms(name)
-
+def assign_rooms(attendees)
+  assignments = []
+  room = 1
+  attendees.each do |name|
+    assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+    room += 1
+  end
+  assignments
 end
